dbcd7b159bdb0e4ccd3b632de00d80fd572b5a16
Adil <kenzhiyevv@gmail.com>
2025-03-15 22:02:07 +0500

    add new blade

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -1,150 +1,109 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Edica :: Home</title>
-    <link rel="stylesheet" href="{{asset('assets/vendors/flag-icon-css/css/flag-icon.min.css')}}">
-    <link rel="stylesheet" href="{{('assets/vendors/font-awesome/css/all.min.css')}}">
-    <link rel="stylesheet" href="{{('assets/vendors/aos/aos.css')}}">
-    <link rel="stylesheet" href="{{('assets/css/style.css')}}">
-    <script src="{{('assets/vendors/jquery/jquery.min.js')}}"></script>
-    <script src="{{('assets/js/loader.js')}}"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | Dashboard</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{'plugins/fontawesome-free/css/all.min.css'}}">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Tempusdominus Bootstrap 4 -->
+    <link rel="stylesheet" href="{{'plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css'}}">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="{{'plugins/icheck-bootstrap/icheck-bootstrap.min.css'}}">
+    <!-- JQVMap -->
+    <link rel="stylesheet" href="{{'plugins/jqvmap/jqvmap.min.css'}}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="dist/css/adminlte.min.css">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href="{{'plugins/overlayScrollbars/css/OverlayScrollbars.min.css'}}">
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href="{{'plugins/daterangepicker/daterangepicker.css'}}">
+    <!-- summernote -->
+    <link rel="stylesheet" href="{{'plugins/summernote/summernote-bs4.min.css'}}">
 </head>
-<body>
-<div class="edica-loader"></div>
-<header class="edica-header">
-    <div class="container">
-        <nav class="navbar navbar-expand-lg navbar-light">
-            <a class="navbar-brand" href="index.html"><img src="assets/images/logo.svg" alt="Edica"></a>
-            <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#edicaMainNav" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="edicaMainNav">
-                <ul class="navbar-nav mx-auto mt-2 mt-lg-0">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="about.html">About</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="blogDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Blog</a>
-                        <div class="dropdown-menu" aria-labelledby="blogDropdown">
-                            <a class="dropdown-item" href="blog.html">Blog Archive</a>
-                            <a class="dropdown-item" href="blog-single.html">Blog Post</a>
-                        </div>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pages</a>
-                        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-                            <a class="dropdown-item" href="404.html">404</a>
-                            <a class="dropdown-item" href="coming-soon.html">Coming Soon</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="contact.html">Contact</a>
-                    </li>
-                </ul>
-                <ul class="navbar-nav mt-2 mt-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"><span class="flag-icon flag-icon-squared rounded-circle flag-icon-gb"></span> Eng</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Download</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </div>
-</header>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
 
-@yield('content')
+    <!-- Preloader -->
+    <div class="preloader flex-column justify-content-center align-items-center">
+        <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+    </div>
 
-<section class="edica-footer-banner-section">
-    <div class="container">
-        <div class="footer-banner" data-aos="fade-up">
-            <h1 class="banner-title">Download it now.</h1>
-            <div class="banner-btns-wrapper">
-                <button class="btn btn-success"> <img src="assets/images/apple@1x.svg" alt="ios" class="mr-2"> App Store</button>
-                <button class="btn btn-success"> <img src="assets/images/android@1x.svg" alt="android" class="mr-2"> Google Play</button>
-            </div>
-            <p class="banner-text">Add some helper text here to explain the finer details of your <br> product or service.</p>
+    <!-- Navbar -->
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+        <!-- Left navbar links -->
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+            </li>
+        </ul>
+    </nav>
+    <!-- /.navbar -->
+
+    <!-- Main Sidebar Container -->
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+
+
+        <!-- Sidebar -->
+        <div class="sidebar">
         </div>
-    </div>
-</section>
-<footer class="edica-footer" data-aos="fade-up">
-    <div class="container">
-        <div class="row footer-widget-area">
-            <div class="col-md-3">
-                <a href="index.html" class="footer-brand-wrapper">
-                    <img src="assets/images/logo.svg" alt="edica logo">
-                </a>
-                <p class="contact-details">hello@edica.com</p>
-                <p class="contact-details">+23 3000 000 00</p>
-                <nav class="footer-social-links">
-                    <a href="#!"><i class="fab fa-facebook-f"></i></a>
-                    <a href="#!"><i class="fab fa-twitter"></i></a>
-                    <a href="#!"><i class="fab fa-behance"></i></a>
-                    <a href="#!"><i class="fab fa-dribbble"></i></a>
-                </nav>
-            </div>
-            <div class="col-md-3">
-                <nav class="footer-nav">
-                    <a href="#!" class="nav-link">Company</a>
-                    <a href="#!" class="nav-link">Android App</a>
-                    <a href="#!" class="nav-link">ios App</a>
-                    <a href="#!" class="nav-link">Blog</a>
-                    <a href="#!" class="nav-link">Partners</a>
-                    <a href="#!" class="nav-link">Careers</a>
-                </nav>
-            </div>
-            <div class="col-md-3">
-                <nav class="footer-nav">
-                    <a href="#!" class="nav-link">FAQ</a>
-                    <a href="#!" class="nav-link">Reporting</a>
-                    <a href="#!" class="nav-link">Block Storage</a>
-                    <a href="#!" class="nav-link">Tools & Integrations</a>
-                    <a href="#!" class="nav-link">API</a>
-                    <a href="#!" class="nav-link">Pricing</a>
-                </nav>
-            </div>
-            <div class="col-md-3">
-                <div class="dropdown footer-country-dropdown">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" id="footerCountryDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <span class="flag-icon flag-icon-gb flag-icon-squared"></span> United Kingdom <i class="fas fa-chevron-down ml-2"></i>
-                    </button>
-                    <div class="dropdown-menu" aria-labelledby="footerCountryDropdown">
-                        <button class="dropdown-item" href="#">
-                            <span class="flag-icon flag-icon-us flag-icon-squared"></span> United States
-                        </button>
-                        <button class="dropdown-item" href="#">
-                            <span class="flag-icon flag-icon-au flag-icon-squared"></span> Australia
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="footer-bottom-content">
-            <nav class="nav footer-bottom-nav">
-                <a href="#!">Privacy & Policy</a>
-                <a href="#!">Terms</a>
-                <a href="#!">Site Map</a>
-            </nav>
-            <p class="mb-0">© Edica. 2020 <a href="https://www.bootstrapdash.com" target="_blank" rel="noopener noreferrer" class="text-reset">bootstrapdash</a> . All rights reserved.</p>
-        </div>
-    </div>
-</footer>
-<script src="assets/vendors/popper.js/popper.min.js"></script>
-<script src="assets/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
-<script src="assets/vendors/aos/aos.js"></script>
-<script src="assets/js/main.js"></script>
+    </aside>
+
+
+    @yield('content')
+
+
+
+    <footer class="main-footer">
+        <strong>BLOG</strong>
+    </footer>
+
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+        <!-- Control sidebar content goes here -->
+    </aside>
+    <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="{{'plugins/jquery/jquery.min.js'}}"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="{{'plugins/jquery-ui/jquery-ui.min.js'}}"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
-    AOS.init({
-        duration: 1000
-    });
+    $.widget.bridge('uibutton', $.ui.button)
 </script>
+<!-- Bootstrap 4 -->
+<script src="{{'plugins/bootstrap/js/bootstrap.bundle.min.js'}}"></script>
+<!-- ChartJS -->
+<script src="{{'plugins/chart.js/Chart.min.js'}}"></script>
+<!-- Sparkline -->
+<script src="{{'plugins/sparklines/sparkline.js'}}"></script>
+<!-- JQVMap -->
+<script src="{{'plugins/jqvmap/jquery.vmap.min.js'}}"></script>
+<script src="{{'plugins/jqvmap/maps/jquery.vmap.usa.js'}}"></script>
+<!-- jQuery Knob Chart -->
+<script src="{{'plugins/jquery-knob/jquery.knob.min.js'}}"></script>
+<!-- daterangepicker -->
+<script src="{{'plugins/moment/moment.min.js'}}"></script>
+<script src="{{'plugins/daterangepicker/daterangepicker.js'}}"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="{{'plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js'}}"></script>
+<!-- Summernote -->
+<script src="{{'plugins/summernote/summernote-bs4.min.js'}}"></script>
+<!-- overlayScrollbars -->
+<script src="{{'plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js'}}"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="dist/js/demo.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="dist/js/pages/dashboard.js"></script>
 </body>
-
 </html>
