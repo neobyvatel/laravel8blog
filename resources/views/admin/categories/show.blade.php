ea290b04129c3347240c705652a7ed0c11535d11
Adil <kenzhiyevv@gmail.com>
2025-03-17 02:00:58 +0500

    add

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -6,8 +6,16 @@
         <div class="content-header">
             <div class="container-fluid">
                 <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1 class="m-0">{{$category->title}}</h1>
+                    <div class="col-sm-6 d-flex align-items-center">
+                        <h1 class="m-0 mr-2">{{$category->title}}</h1>
+                        <a href="{{route('admin.category.edit',$category->id)}}" class="text-success"><i class="fas fa-pencil-alt"></i></a>
+                        <form action="{{route('admin.category.delete',$category->id)}}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="border-0 bg-transparent">
+                                <i class="fas fa-trash text-danger" role="button"></i>
+                            </button>
+                        </form>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
