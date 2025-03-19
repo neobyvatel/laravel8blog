e0bf96310c0f6ce6a1f9ac38ff41c65ece8b6366
Adil <kenzhiyevv@gmail.com>
2025-03-19 15:21:33 +0500

    users can add posts without tags

--- app/Service/PostService.php
+++ app/Service/PostService.php
@@ -8,45 +8,56 @@ use Illuminate\Support\Facades\Storage;
 
 class PostService
 {
-        public function Store($data){
+    public function Store($data)
+    {
 
-            try {
-                Db::beginTransaction();
+        try {
+            Db::beginTransaction();
+            if (isset($data['tag_ids'])) {
                 $tagIds = $data['tag_ids'];
                 unset($data['tag_ids']);
-
-                if (isset($data['preview_image'])) {
-
-                    $data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image']);
-
-                }
-                if (isset($data[',main_image'])) {
-                    $data['main_image'] = Storage::disk('public')->put('/images', $data['main_image']);
-
-                }
-                $post = Post::firstOrCreate($data);
-                $post->tags()->attach($tagIds);
-                Db::commit();
-            } catch (\Exception $exception) {
-                Db::rollBack();
-                abort(404);
             }
-        }
-        public function update($data,$post){
-            try {
-                DB::beginTransaction();
-                $tagIds = $data['tag_ids'];
-                unset($data['tag_ids']);
+
+            if (isset($data['preview_image'])) {
 
                 $data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image']);
-                $data['main_image'] = Storage::disk('public')->put('/images', $data['main_image']);
-                $post->update($data);
-                $post->tags()->sync($tagIds);
-                DB::commit();
-            } catch (\Exception $exception) {
-                DB::rollBack();
-                abort(500);
+
             }
-            return $post;
+            if (isset($data[',main_image'])) {
+                $data['main_image'] = Storage::disk('public')->put('/images', $data['main_image']);
+
+            }
+            $post = Post::firstOrCreate($data);
+            if (isset($tagIds)) {
+                $post->tags()->attach($tagIds);
+            }
+            Db::commit();
+        } catch (\Exception $exception) {
+            Db::rollBack();
+            abort(404);
         }
+    }
+
+    public function update($data, $post)
+    {
+        try {
+            DB::beginTransaction();
+            if (isset($data['tag_ids'])) {
+                $tagIds = $data['tag_ids'];
+                unset($data['tag_ids']);
+            }
+
+            $data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image']);
+            $data['main_image'] = Storage::disk('public')->put('/images', $data['main_image']);
+            $post->update($data);
+            if (isset($tagIds)) {
+                $post->tags()->sync($tagIds);
+            }
+            DB::commit();
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            abort(500);
+        }
+        return $post;
+    }
 }
