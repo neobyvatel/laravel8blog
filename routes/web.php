8dec5bdc453d4d2168f354399b5868398b4bb6ec
Adil <kenzhiyevv@gmail.com>
2025-03-20 20:29:10 +0500

    add Middleware

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,7 +18,7 @@ Route::group(['namespace' => 'Main'], function () {
     Route::get('/', 'IndexController');
 
 });
-Route::group(['namespace' => 'Admin','prefix' => 'admin' ], function () {
+Route::group(['namespace' => 'Admin','prefix' => 'admin', 'middleware'=> ['auth','admin']], function () {
     Route::group(['namespace' => 'Main'], function () {
         Route::get('/', 'IndexController');
     });
