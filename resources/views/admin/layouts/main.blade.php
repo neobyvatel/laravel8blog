1341693c800ea60961b3731c4162f62424b65291
Adil <kenzhiyevv@gmail.com>
2025-03-29 22:39:27 +0500

    debugging

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -116,11 +116,11 @@
 
 
 <!-- AdminLTE App -->
-<script src="dist/js/adminlte.js"></script>
+<script src="{{asset('dist/js/adminlte.js')}}"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="dist/js/demo.js"></script>
+<script src="{{asset('dist/js/demo.js')}}"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="dist/js/pages/dashboard.js"></script>
+<script src="{{asset('dist/js/pages/dashboard.js')}}"></script>
 <script>
     $(document).ready(function (){
         $('#summernote').summernote({
